43678f06c139aafba331fb33ddb72462cd548fa7
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2020-03-03 16:49:36 +0100

    Refactor Slack ID matching

--- src/airtable.ts
+++ src/airtable.ts
@@ -1,25 +1,33 @@
 import Airtable from 'airtable';
 import { Volunteer } from './slack';
 
+export async function getVolunteerAirTableId(table: Airtable.Table<{}>, slackId: string): Promise<string | null> {
+    const filter = `SEARCH("${slackId}", {slackId})`
+    const query = table.select({
+        filterByFormula: filter,
+        fields: ["slackId"],
+    })
+    const matches = await query.firstPage()
+    return (matches.length > 0) ?
+        matches[0].id :
+        null
+}
+
 export async function saveToAirTable(apiToken: string, volunteers: Volunteer[]) {
 
     const table = new Airtable({apiKey: apiToken}).base('app98Yx4PLPDr68df')('Volunteers')
+
     var count = 1
 
     for (const volunteer of volunteers) {
         try {
-            const filter = `SEARCH("${volunteer.slackId}", {slackId})`
-            const query = table.select({
-                filterByFormula: filter,
-                fields: ["slackId"],
-            })
-            const matches = await query.firstPage()
-            if (matches.length > 0) {
-                const existing = matches[0]
-                console.log(`Updating existing user “${volunteer.slackId}”, record ${count}/${volunteers.length}.`)
-                await table.update(existing.id, volunteer)
+            const slackId = volunteer.slackId
+            const existingId = await getVolunteerAirTableId(table, slackId)
+            if (existingId != null) {
+                console.log(`Updating existing user “${slackId}”, record ${count}/${volunteers.length}.`)
+                await table.update(existingId, volunteer)
             } else {
-                console.log(`Inserting new user “${volunteer.slackId}”, record ${count}/${volunteers.length}.`)
+                console.log(`Inserting new user “${slackId}”, record ${count}/${volunteers.length}.`)
                 await table.create(volunteer)
             }
         } catch (err) {
